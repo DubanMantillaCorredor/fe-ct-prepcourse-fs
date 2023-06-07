156137347c4aa8c14447aa6b176299e5dda28142
DubanMantillaCorredor <dubanmantilla@gmail.com>
2023-06-07 15:58:59 -0500

    homework javascript básico

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -4,6 +4,8 @@
 function devolverString(string) {
    // Debe retornar un string.
    // Tu código:
+   miString = string;
+   return miString;
 }
 
 // ⛔️ "X" e "Y" son números.
@@ -11,26 +13,36 @@ function devolverString(string) {
 function suma(x, y) {
    // Retorna el resultado de su suma.
    // Tu código:
+   sum = x + y;
+   return sum;
 }
 
 function resta(x, y) {
    // Retorna el resultado de la resta.
    // Tu código:
+   resultado = x - y;
+   return resultado;
 }
 
 function divide(x, y) {
    // Retorna el resultado de su división.
    // Tu código:
+   resultado = x / y;
+   return resultado;
 }
 
 function multiplica(x, y) {
    // Retorna el resultado de su multiplicación.
    // Tu código:
+   resultado = x * y;
+   return resultado;
 }
 
 function obtenerResto(x, y) {
    // Obten el resto de la división de "x" entre "y".
    // Tu código:
+   resultado = x % y;
+   return resultado
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
